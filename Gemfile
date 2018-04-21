93bf7fadb5baf62e88d4c38ec4520f77fd744cc2
EC2 Default User <ec2-user@ip-172-31-9-26.us-east-2.compute.internal>
2018-04-21 21:46:04 +0000

    change gemfile

--- Gemfile
+++ Gemfile
@@ -26,8 +26,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :production do
-  gem 'pg'
-  gem 'rails_12factor'
+gem 'pg', '~> 0.18.4'
 end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :primes
+  root 'primes#index'
   get '/check', :controller=>'primes', :action=>'checkprime'
   post '/validate', :controller=>'primes', :action=>'isprime'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
